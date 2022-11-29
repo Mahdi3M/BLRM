4fcaa4eb10ec02baac3fb635efb2a7df88e219de
Mahdi3M <mahdimohtasim@gmail.com>
2022-11-29 18:02:18 +0600

    nav bar added

---
+++ header.php
@@ -0,0 +1,16 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <img src="assets/img/logo.png" alt="">
+    <a class="navbar-brand" href="#">Government of the<br>People's Republic of Bangladesh</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Sign In</a>
+          <a class="nav-link" href="#">Contacts</a>
+      </div>
+    </div>
+  </div>
+</nav>
