e533e6cedacfc38c551978f57b3f7b7d92643155
Mahdi3M <mahdimohtasim@gmail.com>
2022-12-05 21:50:02 +0600

    admin-pages

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+    <div class="position-sticky">
+    <div class="list-group list-group-flush mx-3 mt-4">
+        <a href="admincontrol.php" class="list-group-item list-group-item-action py-2 ripple" aria-current="true" id="admin_control">
+        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Admin Control</span>
+        </a>
+        <a href="viewsection.php" class="list-group-item list-group-item-action py-2 ripple" id="view_section"><i
+            class="fas fa-chart-bar fa-fw me-3"></i><span>View Section</span></a>
+        <a href="adduser.php" class="list-group-item list-group-item-action py-2 ripple" id="add_user"><i
+            class="fas fa-users fa-fw me-3"></i><span>Add User</span></a>
+    </div>
+    </div>
+</nav>
